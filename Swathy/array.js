0a02714c2467dfb937d99cb004a340335f7f7a4e
Swathi964 <115422557+Swathi964@users.noreply.github.com>
2024-09-03 10:44:35 +0530

    Add files via upload

---
+++ Swathy/array.js
@@ -0,0 +1,5 @@
+let fruits=['apple','orange','banana'];
+console.log(fruits);
+console.log(fruits[1]);
+fruits.push('kiwi');
+console.log(fruits);
---
+++ Swathy/basic.js
@@ -0,0 +1,14 @@
+console.log("temperature")
+temp=45
+if(temp<20)
+{
+    console.log("cold")
+}
+else if(temp<=20 &&temp<=30)
+{
+    console.log("normal")
+}
+else
+{
+    console.log("hot day")
+}
